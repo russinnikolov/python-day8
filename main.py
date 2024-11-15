8caa47275915fdab2b801408ec340e33ecd35178
Rusin <russinnikolov@gmail.com>
2024-11-15 14:06:10 +0200

    a little bit more to go

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-alphabet_count = len(alphabet)
+
+
+def ceaser(original_text, shift_number, operation):
+    output = ""
+    for char in original_text:
+        if char in alphabet:
+            if operation == 'decode':
+                shift_number *= -1
+
+            new_position = alphabet.index(char) + shift_number
+            new_position %= len(alphabet)
+            output += alphabet[new_position]
+        else:
+            output += char
+
+    word = "encoded" if operation == "encode" else "decoded"
+    print(f"The {word} text is {output}")
 
 continue_program = "yes"
 while continue_program == "yes":
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    if(direction not in ["encode", "decode"]):
+    if (direction not in ["encode", "decode"]):
         print("Invalid input")
         continue
 
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
-    direction(text, shift)
+    ceaser(text, shift, direction)
 
     continue_program = input("Type 'yes' if you want to go again. Otherwise type 'no'")
-    if(continue_program not in ["yes", "no"]):
+    if (continue_program not in ["yes", "no"]):
         print("Invalid input")
         continue
-
-
-def encrypt(text, shift):
-    encrypted_text = ""
-    for char in text:
-        if char in alphabet:
-            new_position = (alphabet.index(char) + shift) % alphabet_count
-            encrypted_text += alphabet[new_position]
-        else:
-            encrypted_text += char
-
-    print(f"The encoded text is {encrypted_text}")
-
-
-# encrypt(text, shift)
